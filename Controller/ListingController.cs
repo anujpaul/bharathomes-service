f3bef8a07f12108bbf9b9873de058a92553177d8
Anuj Paul <anujpaul19@outlook.com>
2026-04-22 16:42:41 -0500

    Adding toeken Auth

--- Controller/ListingController.cs
+++ Controller/ListingController.cs
@@ -114,81 +114,38 @@ public class ListingController : ControllerBase
         }
         var userId    = User.FindFirst(ClaimTypes.NameIdentifier)?.Value
                     ?? User.FindFirst("sub")?.Value;
-    var userName  = User.FindFirst(ClaimTypes.GivenName)?.Value
-                    ?? User.FindFirst("given_name")?.Value;
-    var userEmail = User.FindFirst(ClaimTypes.Email)?.Value
-                    ?? User.FindFirst("email")?.Value;
+        var userName  = User.FindFirst(ClaimTypes.GivenName)?.Value
+                        ?? User.FindFirst("given_name")?.Value;
+        var userEmail = User.FindFirst(ClaimTypes.Email)?.Value
+                        ?? User.FindFirst("email")?.Value;
 
-    _logger.LogInformation("UserId: {id}, Name: {name}, Email: {email}", 
-        userId, userName, userEmail);
-        // var (userId, userName, userEmail) = GetUserFromHeader();
+        _logger.LogInformation("UserId: {id}, Name: {name}, Email: {email}", 
+            userId, userName, userEmail);
 
-        // // var MicrosoftPrincipalId = Request.Headers["X-MS-CLIENT-PRINCIPAL"].FirstOrDefault();
-        // // var MicrosoftPrincipalName = Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"].FirstOrDefault();
-        // // var userId = Request.Headers["X-User-Id"].FirstOrDefault();
-        // // var userName = Request.Headers["X-User-Name"].FirstOrDefault();
-        // // var userEmail = Request.Headers["X-User-Email"].FirstOrDefault();
-
-        
-        // // _logger.LogInformation($"MicrosoftPrincipalName : {MicrosoftPrincipalName}");
-
-        // if (!string.IsNullOrEmpty(userId))
-        // {
-        //     _logger.LogInformation($"MicrosoftId Id : {userId}");
-        //     UserProfile user = new UserProfile
-        //                                 {
-        //                                     Id = userId,
-        //                                     Name = userName ?? "",
-        //                                     Email = userEmail ?? ""
-        //                                 };
-        //     var userProfile = await _cosmosService.ReadItemAsync<UserProfile>(userId);
-
-        //     if (userProfile == null)
-        //     {
-        //         await _cosmosService.CreateItemAsyc<UserProfile>(user);
-        //         _logger.LogInformation("User Created");
-        //         return Created("", new { message = "User created" });
-        //     }
-        // }
-
-        // _logger.LogInformation($"User Id : {userName}");
-
-        
-
-
-
-        return Ok(new { message = "User exists" });
-        
-    }
-
-
-    private (string? id, string? name, string? email) GetUserFromHeader()
-    {
-        var principalHeader = Request.Headers["X-MS-CLIENT-PRINCIPAL"].FirstOrDefault();
-
-        _logger.LogInformation($"MicrosoftPrincipal Header  : {principalHeader}");
-
-        if (string.IsNullOrEmpty(principalHeader))
-            return (null, null, null);
-
-        var json = Encoding.UTF8.GetString(Convert.FromBase64String(principalHeader));
-        var doc = JsonDocument.Parse(json);
-
-        string? id = null;
-        string? name = null;
-        string? email = null;
-
-        foreach (var claim in doc.RootElement.GetProperty("claims").EnumerateArray())
+        if (!string.IsNullOrEmpty(userId))
         {
-            var type = claim.GetProperty("typ").GetString();
-            var value = claim.GetProperty("val").GetString();
+            _logger.LogInformation($"MicrosoftId Id : {userId}");
+            UserProfile user = new UserProfile
+                                        {
+                                            Id = userId,
+                                            Name = userName ?? "",
+                                            Email = userEmail ?? ""
+                                        };
+            var userProfile = await _cosmosService.ReadItemAsync<UserProfile>(userId);
 
-            if (type == "nameidentifier") id = value;
-            if (type == "name") name = value;
-            if (type == "emailaddress") email = value;
+            if (userProfile == null)
+            {
+                await _cosmosService.CreateItemAsyc<UserProfile>(user);
+                _logger.LogInformation("User Created");
+                return Created("", new { message = "User created" });
+            }
+            else
+            {
+                return Ok(new { message = "User exists" });
+            }
         }
-
-        return (id, name, email);
+        return Ok(new { message = "Something went wrong" });
+        
     }
 
 }
