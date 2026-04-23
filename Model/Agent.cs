de029fc8b074770f7c03b51ea693b8cecfafd1a9
Anuj Paul <anujpaul19@outlook.com>
2026-04-22 20:51:21 -0500

    Adding toeken Auth

--- Model/Agent.cs
+++ Model/Agent.cs
@@ -11,6 +11,8 @@ public class Agent
     public string Name { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
     public double Rating { get; set; }
     public int ListingsCount { get; set; }
     public string Specialization { get; set; } = string.Empty;
