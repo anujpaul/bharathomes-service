817eb9878ee5f5894fefbcd365da928842ecefcf
Anuj Paul <anujpaul19@outlook.com>
2026-05-10 14:12:42 -0500

    Added mock payment endpoints.

---
+++ Migrations/20260510034351_AddReraDocumentUrlToProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bharathome_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReraDocumentUrlToProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "rera_document_url",
+                table: "properties",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "rera_document_url",
+                table: "properties");
+        }
+    }
+}
